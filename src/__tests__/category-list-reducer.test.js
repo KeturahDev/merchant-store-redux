e7855e7e336c87f61feafe4abefa9e9a70499d5e
Keturah Howard <keturah.dev@gmail.com>
2020-04-27 14:44:53 -0700

    created and passed test for adding categories.
    Co-authored-by: jeremy kale padot <jeremy@padot.us>
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>

--- src/__tests__/category-list-reducer.test.js
+++ src/__tests__/category-list-reducer.test.js
@@ -2,8 +2,36 @@ import categoryListReducer from "./../reducers/category-list-reducer";
 
 describe("categoryListReducer", () => {
 
+  let action;
+  const categoryData = {
+    name: "shoes",
+    id: 1,
+    items: [
+      {
+        itemName: "Everyday Sneakers",
+        description: "so comfy oooo",
+        quantity: 10,
+        id: 1
+      }]
+  };
+  
   test("Should return an empty state", () => {
     expect(categoryListReducer({}, {type: null})).toEqual({});
   })
-
+  test("Should add a new category", () => {
+    const {name, id, items} = categoryData;
+    action = {
+      type: "ADD_CATEGORY",
+      name: name,
+      id: id,
+      items: items
+    };
+    expect(categoryListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        id: id,
+        items: items
+      }
+    })
+  })
 })
--- src/reducers/category-list-reducer.js
+++ src/reducers/category-list-reducer.js
@@ -1,3 +1,15 @@
 export default (state = {}, action) => {
-  return state;
+  const {name, id, items} = action
+  switch (action.type) {
+    case 'ADD_CATEGORY':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          id: id,
+          items: items
+        }
+      });
+    default:
+      return state;
+  }
 };
